6b32256a4e1a23f2efe142ad38a52c22c2045fff
Dragos Barbuta <barbuta.dragos12@gmail.com>
2024-04-20 15:35:09 +0200

    data loading exercise

---
+++ exercise/02-data-loading/app/models/post.server.ts
@@ -0,0 +1,13 @@
+export async function getPosts() {
+  const data = [
+    {
+      slug: "my-first-post",
+      title: "My First Post",
+    },
+    {
+      slug: "90s-mixtape",
+      title: "A Mixtape I Made Just For You",
+    },
+  ];
+  return data;
+}
