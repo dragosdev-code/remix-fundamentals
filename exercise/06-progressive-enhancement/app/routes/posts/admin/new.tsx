a0170713cc8fd490f50b789d085cad29f3bcd349
Dragos Barbuta <barbuta.dragos12@gmail.com>
2024-04-21 15:21:27 +0200

    Progressive enhancement and prefetching exercise

--- exercise/06-progressive-enhancement/app/routes/posts/admin/new.tsx
+++ exercise/06-progressive-enhancement/app/routes/posts/admin/new.tsx
@@ -1,6 +1,6 @@
 import type { ActionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useTransition } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
 import { createPost } from "~/models/post.server";
@@ -35,6 +35,9 @@ const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`
 
 export default function NewPost() {
   const errors = useActionData<typeof action>();
+  const transition = useTransition();
+
+  const isSubmitting = Boolean(transition.submission);
 
   // 🐨 get the transition from useTransition
   // 🦉 make sure that useTransition comes from "@remix-run/react" and NOT "react"!!
@@ -78,11 +81,12 @@ export default function NewPost() {
       <p className="text-right">
         <button
           type="submit"
-          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isSubmitting}
           // 🐨 add a disabled attribute to the button if we're creating
         >
           {/* 🐨 Have this say "Creating..." if we're in a creating state */}
-          Create Post
+          {isSubmitting ? "Creating..." : "Create Post"}
         </button>
       </p>
     </Form>
